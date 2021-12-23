575d1c586d7a30c00abdc7f56729ec41cd1d2b99
senthuran16 <senthuranrc16@gmail.com>
2021-12-24 00:54:36 +0530

    Update elasticsearch version to 7.16.2

--- component/src/main/java/io/siddhi/extension/store/elasticsearch/ElasticsearchEventTable.java
+++ component/src/main/java/io/siddhi/extension/store/elasticsearch/ElasticsearchEventTable.java
@@ -66,9 +66,10 @@ import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.unit.ByteSizeUnit;
 import org.elasticsearch.common.unit.ByteSizeValue;
-import org.elasticsearch.common.unit.TimeValue;
-import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.core.TimeValue;
+import org.elasticsearch.xcontent.XContentBuilder;
+import org.elasticsearch.xcontent.XContentFactory;
+
 
 import java.io.IOException;
 import java.io.InputStream;
